bf2e8a2316cc83876bcaae5cd150fa6d9523fe1e
John DeSilva <desilvjo@umich.edu>
2023-12-10 16:54:11 -0500

    Refactor

--- 2023/7/b.rb
+++ 2023/7/b.rb
@@ -20,25 +20,19 @@ module JokerWildcard
   private
 
   def best_hand
+    new_cards = cards.map { |card| card.joker? ? best_non_joker_card : card }
+
+    self.class.new bid:, cards: new_cards
+  end
+
+  def best_non_joker_card
     non_joker_groups = groups.reject { _1.any? &:joker? }
+
+    return Card.new 'A' if non_joker_groups.empty?
 
     best_group = non_joker_groups.sort_by { [_1.size, _1] }.last
 
-    best_card = if best_group
-                  best_group.first
-                else
-                  Card.new 'A'
-                end
-
-    new_cards = cards.map do |card|
-      if card.joker?
-        best_card
-      else
-        card
-      end
-    end
-
-    self.class.new bid:, cards: new_cards
+    best_group.first
   end
 end
 
